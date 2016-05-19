ecd869eed16c3aa28970fb60180a0b90509f28a6
Peter Neupauer <peponeupauer@gmail.com>
2016-05-19 09:42:31 +0200

    Admin Hello World

---
+++ menu-admin/src/mani/java/cz/muni/fi/pb138/App.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pb138;
+
+import static spark.Spark.get;
+
+/**
+ * @author Peter Neupauer
+ */
+public class App {
+
+    public static void main(String[] args) {
+        get("/", (request, response) -> "Hello World!");
+    }
+}
