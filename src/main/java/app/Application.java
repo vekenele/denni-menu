85185fec50d1cc965353fdfe3f9dab13896d65e2
Peter Neupauer <peponeupauer@gmail.com>
2016-06-20 13:38:06 +0200

    add comments to Application.java

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,4 @@
 package app;
-/**
- * Created by Jiri Ketner on 17/06/16.
- */
 
 import app.admin.AdminController;
 import app.restaurant.RestaurantController;
@@ -13,8 +10,21 @@ import spark.template.pebble.PebbleTemplateEngine;
 
 import static spark.Spark.*;
 
+/**
+ * Main Application class.
+ *
+ * @author Bc. Jiří Ketner
+ * @author Bc. David Věžník
+ * @author Peter Neupauer
+ * @version 1.0
+ */
 public class Application {
 
+    /**
+     * Here it is where all routes are registered for application.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
 
         PebbleTemplateEngine engine = new PebbleTemplateEngine(getLoader());
@@ -45,6 +55,11 @@ public class Application {
 
     }
 
+    /**
+     * Creates new instance of ClasspathLoader.
+     *
+     * @return class path loader
+     */
     private static Loader getLoader() {
         Loader loader = new ClasspathLoader();
         loader.setPrefix("templates/");
